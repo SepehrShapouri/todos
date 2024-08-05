96b169254bf1f66b478e663fa68f2e64e889305d
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-08-05 13:44:57 +0330

    feat:added login page design

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,7 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -6,24 +9,55 @@ import React from "react";
 function page() {
   return (
     <MaxWidthWrapper>
-      <section className="p-[2rem] min-h-[85dvh] flex flex-col items-center justify-between">
-        <Image
+      <section className="p-[2rem] min-h-[95dvh] flex flex-col items-center  justify-between">
+<div>
+<Image
           src="/images/auth.jpg"
           width={2000}
           height={2000}
           alt="auth page"
-        />
-        <div className="flex flex-col gap-[12px]">
-          <h1 className="font-extrabold text-sky-200 text-3xl text-center">TODOS</h1>
-          <p className="text-gray-900/70 text-sm text-center">
-            Easily manage and get your stuff done with TODOS
-          </p>
-        </div>
-        <div>
-        <span className="text-gray-900/80">
-          Login or <Link href="/signup" className="text-blue-500">Create and account</Link> to get started.
-        </span>
-        
+        />         <div className="flex flex-col gap-[12px]">
+        <h1 className="font-extrabold text-sky-200 text-3xl text-center">
+          TODOS
+        </h1>
+        <p className="text-gray-900/70 text-sm text-center">
+          Easily manage and get your stuff done with TODOS
+        </p>
+      </div>
+</div>
+        <div className="flex flex-col gap-[1rem]">
+          <div className="flex w-full justify-center">
+            <span className="text-gray-900/60 text-center">
+              Login or{" "}
+              <Link href="/signup" className="text-blue-400">
+                Create and account
+              </Link>{" "}
+              to get started.
+            </span>
+          </div>
+          <form className="flex flex-col gap-[1rem]">
+            <span>
+              <Label className="text-gray-600 text-sm" htmlFor="username">
+                Username
+              </Label>
+              <Input
+                placeholder="your username"
+                id="username"
+                name="username"
+              />
+            </span>
+            <span>
+              <Label className="text-gray-600 text-sm" htmlFor="password">
+                Password
+              </Label>
+              <Input
+                placeholder="your password"
+                id="password"
+                name="password"
+              />
+            </span>
+            <Button className="bg-sky-200 w-full">Continue</Button>
+          </form>
         </div>
       </section>
     </MaxWidthWrapper>
