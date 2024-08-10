a8cdb6d03b709d8e002bc53c7bcf682a2aad23e1
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2024-08-10 23:26:44 +0330

    fix:fix prisma build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
+//@ts-check
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
 export default withPWA({
